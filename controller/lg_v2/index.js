30ae43d2323ffd0389c1e2febeab5b32846b7d6a
ANN MYONG JIN <audwlsd7782@naver.com>
2019-11-03 16:38:04 +0900

    router 협업 할 수 있도록 설정 완료

--- controller/lg_v2/index.js
+++ controller/lg_v2/index.js
@@ -1,6 +1,41 @@
 const admin_ctrl = require('./admin.ctrl')
 const router = express.Router()
-
+// 명진이
 router.post('/user/create', admin_ctrl.create_user)
 router.post('/data/delete', admin_ctrl.delete_header_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 종성이
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router
