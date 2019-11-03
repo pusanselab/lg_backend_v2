dab760789ce08b4ef85a79784841a29fcc99e776
dotols <lim00010@naver.com>
2019-11-03 22:05:34 +0900

    chamber status API complete

--- controller/lg_v2/chamber.ctrl.js
+++ controller/lg_v2/chamber.ctrl.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+
 const recent_test = (req, res) => {
     const result = {}
 
@@ -22,26 +24,38 @@ const recent_test = (req, res) => {
 
 
 const chamber_status = (req, res) => {
-    const result = {}
-    // const date = new Date();
-    //
-    // console.log(date)
+    const result = {
+        month:[
+            [],[],[],[],[],[]
+        ]
+    }
+    const date = new Date();
+
     db.Header.findAll({
         order: [
             ['lgmv_date', 'DESC']
         ],
-        // limit: 5
+        attributes:[ 'conn_file_date' ]
     }).then(header => {
         if (header.length == null) {
             result.code = 400
             result.message = "failure"
             return res.json(result)
         } else {
-            console.log(header)
-            result.content = header
+
+            for(var i = 0 ; i < header.length ; i++){
+                var data_date = header[i].dataValues.conn_file_date;
+                data_date = data_date.replace(/\./g,'-')
+                console.log(data_date)
+                var between = moment(date).diff(data_date, "month")
+
+                if( between < 6 ){
+                    result.month[between].push(header[i].dataValues)
+                }
+            }
+
             result.code = 200
             result.message = "success"
-            console.log(result)
             return res.json(result)
         }
     })
