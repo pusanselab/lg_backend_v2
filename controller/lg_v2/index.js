8a9e82ad040686ccd5a3a9a8751e87ac79fa5552
ANN MYONG JIN <audwlsd7782@naver.com>
2019-11-03 16:57:12 +0900

    협업 컨트롤러, 라우팅 작업 설정 완료

--- controller/lg_v2/index.js
+++ controller/lg_v2/index.js
@@ -1,8 +1,27 @@
+// 명진 컨트롤러
 const admin_ctrl = require('./admin.ctrl')
+const data_search_ctrl = require('./data_search.ctrl')
+// const data_search_ctrl = require('./data_search.ctrl')
+// const data_search_ctrl = require('./data_search.ctrl')
+// const data_search_ctrl = require('./data_search.ctrl')
+// 종성 컨트롤러
+// const data_search_ctrl = require('./data_search.ctrl')
+// const data_search_ctrl = require('./data_search.ctrl')
+// const data_search_ctrl = require('./data_search.ctrl')
+// const data_search_ctrl = require('./data_search.ctrl')
+// const data_search_ctrl = require('./data_search.ctrl')
 const router = express.Router()
+
+
+
+
+
+
 // 명진이
 router.post('/user/create', admin_ctrl.create_user)
 router.post('/data/delete', admin_ctrl.delete_header_data)
+router.post('/data_search_id', data_search_ctrl.data_search_id)
+router.post('/data_search', data_search_ctrl.data_search)
 
 
 
@@ -19,6 +38,7 @@ router.post('/data/delete', admin_ctrl.delete_header_data)
 
 
 // 종성이
+
 
 
 
