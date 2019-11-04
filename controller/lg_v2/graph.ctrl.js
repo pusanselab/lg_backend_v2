d7f7a8aa08e5b29d8c65456e45aa2bc58ab6ebf4
ANN MYONG JIN <audwlsd7782@naver.com>
2019-11-04 20:18:03 +0900

    graph api -> object 합치는 걸로 소스코드 수정 완료, Json 포멧 형식 변경

--- controller/lg_v2/graph.ctrl.js
+++ controller/lg_v2/graph.ctrl.js
@@ -3,7 +3,7 @@ const get_item_list = (req, res) => {
 
     db.Item.findAll({
         where: {
-            header_uid : header_uid
+            header_uid: header_uid
         }
     }).then(items => {
         return res.json(items)
@@ -13,80 +13,99 @@ const get_item_list = (req, res) => {
 const get_raw_list = async (req, res) => {
     const header_uid = req.body.header_uid
     const item_list = req.body.item_list
-    const result = []
-    var i =0
+    const result = {
+            items: [],
+            raws: {},
+            time: {}
+    }
+    var i = 0
     const temp_time = {}
+    const temp_result = []
     await db.Raw_1.findAll({
         where: {
             header_uid: header_uid
         },
-        attributes: ['Date','time']
+        attributes: ['Date', 'time']
     }).then(async dateAndTimes => {
-        temp_time.time = dateAndTimes
-        await result.push(temp_time)
+        result.time = dateAndTimes
     })
-     for (i; i<item_list.length; i++) {
+    for (i; i < item_list.length; i++) {
         var data = {}
-
         item_num = item_list[i].slice(5)
-        if(parseInt(item_num) <= 1000){
+        if (parseInt(item_num) <= 1000) {
             await db.Raw_1.findAll({
                 where: {
                     header_uid: header_uid
                 },
-                attributes: [item_list[i]],
-                limit: 1000
+                attributes: [item_list[i]]
             }).then(async raw_1s => {
                 data.item = item_list[i]
                 data.raws = raw_1s
-                await result.push(data)
+                 result.items.push(item_list[i])
+                 temp_result.push(raw_1s)
+
             })
         }
-        else if(parseInt(item_num) <= 2000){
+        else if (parseInt(item_num) <= 2000) {
             db.Raw_2.findAll({
                 where: {
                     header_uid: header_uid
                 },
-                attributes: [item_list[i]],
-                limit: 1000
+                attributes: [item_list[i]]
 
             }).then(async raw_2s => {
                 data.item = item_list[i]
                 data.raws = raw_2s
-                await result.push(data)
+                 result.items.push(item_list[i])
+                 temp_result.push(raw_2s)
             })
         }
-        else if(parseInt(item_num) <= 3000){
+        else if (parseInt(item_num) <= 3000) {
             db.Raw_3.findAll({
                 where: {
                     header_uid: header_uid
                 },
-                attributes: [item_list[i]],
-                limit: 1000
+                attributes: [item_list[i]]
 
             }).then(async raw_3s => {
                 data.item = item_list[i]
                 data.raws = raw_3s
-                await result.push(data)
+                 result.items.push(item_list[i])
+                 temp_result.push(raw_3s)
             })
         }
-        else if(parseInt(item_num) <= 4000){
+        else if (parseInt(item_num) <= 4000) {
             db.Raw_4.findAll({
                 where: {
                     header_uid: header_uid
                 },
-                attributes: [item_list[i]],
-                limit: 1000
+                attributes: [item_list[i]]
 
             }).then(async raw_4s => {
                 data.item = item_list[i]
                 data.raws = raw_4s
-                await result.push(data)
+                 result.items.push(item_list[i])
+                 temp_result.push(raw_4s)
             })
+
+                raw_new = Object.assign(data.raws)
         }
-         if(i === item_list.length - 1) {
-             return res.json(result)
-         }
+        if (i === item_list.length - 1) {
+            var raws_result = []
+            var size_of_raws = temp_result[0].length
+            for(let k=0; k<size_of_raws; k++){
+                var temp_obj = {}
+                var temp_obj2 = {}
+                for(let j=0; j<temp_result.length; j++){
+                    temp_obj2 = temp_obj
+                    temp_obj = Object.assign(temp_obj2, temp_result[j][k].dataValues)
+                }
+                raws_result.push(temp_obj)
+            }
+
+            result.raws = raws_result
+            return res.json(result)
+        }
     }
 
 }
