cea58e8546c8f71a17e567791ef9f69596c57e10
dotols <lim00010@naver.com>
2019-11-13 23:26:21 +0900

    red_check  API modifing

--- controller/lg_v2/red_check.ctrl.js
+++ controller/lg_v2/red_check.ctrl.js
@@ -36,29 +36,37 @@ const red_check = (req, res) => {
         } else {
             result.code = 200
             result.message = "success"
-            re_arr = []
             for (let i = 0; i < header.length; i++) {
                 result.content.header_uid.push(header.header_uid)
-
-            }
-            const eer_result_params = header[header.length-1].eer_result_params.split(",")
-            const eer_item_num = header[header.length-1].eer_item_num
-
-            const temp = [
-                'Time',
-                [sequelize.fn('MAX', sequelize.col(eer_item_num)), 'EER']
-            ]
-            for(let j = 0 ; j < eer_result_params.length ; j++){
-                console.log(eer_result_params[j])
-                temp.push(eer_result_params[j])
             }
 
-            db.Raw_0000_0500.findOne({
+            const recent_header = header[header.length-1]
+            const eer_result_params = recent_header.eer_result_params.split(",")
+            const eer_item_num = recent_header.eer_item_num
+
+            console.log(eer_item_num)
+            // const temp = [
+            //     'Time',
+            //     [sequelize.fn('MAX', sequelize.col(eer_item_num)), 'EER']
+            // ]
+            // for(let j = 0 ; j < eer_result_params.length ; j++){
+            //     console.log(eer_result_params[j])
+            //     temp.push(eer_result_params[j])
+            // }
+
+            db.Raw_0000_0500.findAll({
                 where:{
-                    header_uid : header[header.length-1].header_uid
+                    header_uid : recent_header.header_uid
                 },
-                attributes:temp
+                attributes:[
+                    [eer_item_num]
+                ],
+                order: [
+                    [eer_item_num , 'DESC']
+                ],
+                limit : 1
             }).then(raw_0000_0500 =>{
+                return res.json(raw_0000_0500)
                 console.log(raw_0000_0500.dataValues)
             })
         }
